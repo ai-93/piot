0585fee27fa9a508e565f76a2414010e7e3c204a
ai-93 <33371682+ai-93@users.noreply.github.com>
2018-10-11 20:56:02 +0530

    Update gpio_web.py

--- gpio_web.py
+++ gpio_web.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify
 from gpio_support import get_gpio_list, setup_gpio, toggle_pin
-from mqtt import MqttService
+
 
 app = Flask(__name__)
 
@@ -12,19 +12,6 @@ def main():
 def gpio_toggle(pin):
     toggle_pin(pin)
     return redirect("/")
-
-@app.route('/mqtt/push', methods=['POST'])
-def mqtt_push():
-    content = request.get_json(silent=True)
-    mqtt = MqttService()
-    print(content)
-    
-    mqtt.message = content['message']
-    mqtt.topic = content['topic']
-    
-    resp = mqtt.push_once()
-    
-    return jsonify({"status":200})
 
 if __name__ == '__main__':
     # setup_gpio() todo: uncomment to setup gpio on start
