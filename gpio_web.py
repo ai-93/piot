a33df98cd9c82d9878c6642c1035f9e6f91e7bae
ai-93 <33371682+ai-93@users.noreply.github.com>
2018-11-09 17:37:44 +0530

    update

--- gpio_web.py
+++ gpio_web.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, redirect, request, jsonify
-from gpio_support import get_gpio_list, setup_gpio, toggle_pin
-from mqtt_support import *
+from gpio_support import get_gpio_list, setup_gpio, get_pin_status, toggle_pin
+from flask_mqtt import Mqtt
+from config_support import db
 
 app = Flask(__name__)
+app.config['MQTT_BROKER_URL'] = db['mqtt']['broker']
+app.config['MQTT_BROKER_PORT'] = db['mqtt']['port']
+app.config['MQTT_USERNAME'] = db['mqtt']['username']
+app.config['MQTT_PASSWORD'] = db['mqtt']['password']
+app.config['MQTT_REFRESH_TIME'] = db['mqtt']['refresh_time']
+mqtt = Mqtt(app)
 
 @app.route('/')
 def main():
@@ -18,4 +25,33 @@ if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
 
 
+@mqtt.on_connect()
+def handle_connect(client, userdata, flags, rc):
+    for item in db['gpio']:
+        mqtt.subscribe("{}/feeds/{}.{}".format(db['mqtt']['username'], db['location'], item['pin']))
+
+
+@mqtt.on_message()
+def handle_mqtt_message(client, userdata, message):
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    print (data)
+    pin = int(message.topic.split(".")[1])
+    state = message.payload.decode()
+
+    if state == "ON":
+        state = 0
+    else:
+        state = 1
+
+    current_state = get_pin_status(pin)
+
+    if state != current_state:
+        toggle_pin(pin)
+
+
+def mqtt_publish(pin, value):
+    mqtt.publish("{}/feeds/{}.{}".format(db['mqtt']['username'], db['location'], pin), value)
 
