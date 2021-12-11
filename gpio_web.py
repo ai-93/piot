1856fb160f3a8f9cc7c44e4cb86e376d21e6afd2
ai-93 <abid.ismail@protonmail.com>
2021-12-11 08:08:21 +0530

    update

--- gpio_web.py
+++ gpio_web.py
@@ -1,18 +1,56 @@
-from flask import Flask, redirect
-from flask_mqtt import Mqtt
+"""
+
+A small Test application to show how to use Flask-MQTT.
+
+"""
+
+import eventlet
 from config_support import db
-import logging
+import json
+from flask import Flask, render_template,redirect
 import sys
+from flask_mqtt import Mqtt
+from flask_socketio import SocketIO
+from flask_bootstrap import Bootstrap
+import logging 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
+eventlet.monkey_patch()
+
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['MQTT_BROKER_URL'] = db['mqtt']['broker']
 app.config['MQTT_BROKER_PORT'] = db['mqtt']['port']
 app.config['MQTT_USERNAME'] = db['mqtt']['username']
 app.config['MQTT_PASSWORD'] = db['mqtt']['password']
 app.config['MQTT_KEEPALIVE'] = 5
-mqtt = Mqtt(app)
+app.config['MQTT_TLS_ENABLED'] = False
 
+mqtt = Mqtt(app)
+socketio = SocketIO(app)
+bootstrap = Bootstrap(app)
+
+
+@app.route('/')
+def index():
+    return redirect("http://pi.hole")
+
+
+@socketio.on('publish')
+def handle_publish(json_str):
+    data = json.loads(json_str)
+    mqtt.publish(data['topic'], data['message'])
+
+
+@socketio.on('subscribe')
+def handle_subscribe(json_str):
+    data = json.loads(json_str)
+    mqtt.subscribe(data['topic'])
+
+
+@socketio.on('unsubscribe_all')
+def handle_unsubscribe_all():
+    mqtt.unsubscribe_all()
 
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
@@ -25,18 +63,17 @@ def handle_connect(client, userdata, flags, rc):
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
-    logging.info(f"{message.topic}")
-    logging.info(f"{message.payload.decode()}")
+    data = dict(
+        topic=message.topic,
+        payload=message.payload.decode()
+    )
+    socketio.emit('mqtt_message', data=data)
 
-@app.route('/')
-def main():
-    return redirect("http://pi.hole")
 
-# @app.route('/tv/state')
-# def get_tv_state():
-#     tv_state = get_current_tv_state()
-#     mqtt.publish("{}/feeds/tv.state".format(db['mqtt']['username']), tv_state)
-#     return str(tv_state)
+@mqtt.on_log()
+def handle_logging(client, userdata, level, buf):
+    print(level, buf)
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8081, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8081, use_reloader=False, debug=True)
